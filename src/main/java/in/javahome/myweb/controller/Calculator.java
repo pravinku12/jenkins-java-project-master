911f75ea385a49514ce56b3cb11467b239c22145
Pravin Kumar <102435455+pravinku12@users.noreply.github.com>
2026-02-17 22:15:56 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -19,7 +19,7 @@ public class Calculator {
 	public int sub(int i, int j){
 		return i-j;
 	}
-	public int add(int i, int j){
+	public int add2(int i, int j){
     System.out.println("Add method executed");
     return i+j;
 }
